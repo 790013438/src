abbf9473302fe99b300c70da846e2a9784846b74
790013438 <790013438@qq.com>
2017-11-22 20:31:56 +0800

    k当成int，初始化StringBuilder容量

--- Join.java
+++ Join.java
@@ -2,5 +2,10 @@ public class Join {
 
     public static void main(String[] args) {
         System.out.println(123456 + 65432l);
+
+        //'k'当成int, 初始化StringBuilder的容量
+        StringBuilder sb = new StringBuilder('k');
+        sb.append('o');
+        System.out.println(sb.toString());
     }
 }
