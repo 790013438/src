37a46da310428e6db562c1e7dd4176fe9430ce4f
790013438 <790013438@qq.com>
2017-11-23 09:25:11 +0800

    assert的使用方法
    启用Assert
        java -enableassertions Assert

---
+++ Assert.java
@@ -0,0 +1,9 @@
+public class Assert {
+
+    public static void main(String[] args) {
+        int a = 0;
+        assert false : "2333, information";
+        assert(a > 0);
+        System.out.println(a > 0);
+    }
+}
