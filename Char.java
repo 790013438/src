c6e7c07f28698de40b2555ea706ad2d4d377a5b5
790013438 <790013438@qq.com>
2017-12-04 14:33:01 +0800

    字面常量

---
+++ Char.java
@@ -0,0 +1,32 @@
+public class Char {
+
+    public static void main(String[] args) {
+        /**
+         * true false null - 不是关键字
+         * 字面常量 - literal
+         *  - 整型字面量：11， 011， 0x11, 0b11(二进制), 123_456, 11L
+         *  - 实型字面量：123.45, 123.45F, 1.2345E2(科学计数法)
+         *  - 字符字面量：'a', '\141'(八进制)，'\u9a86\', '\n'
+         *  - boolean字面量：true, false
+         *  - 引用字面量：null
+         *  - 字符串字面量："hello"
+         *  - 类型字面量：String.class
+         * goto const 是不是关键字，是关键字，有的也叫保留字
+         * Java5 50 - 2 = 48
+         * byte short int long float double char boolean
+         * package(必须是第一句话) import
+         * class interface extends implements abstract
+         * new instanceof this super
+         * final static
+         * public protected private
+         * if else switch case default
+         * do while for break continue
+         * return void
+         * try catch finally throw throws
+         * synchronized volatile(防止编译器优化假设)
+         * native（c语言JNI - Java Native Interface） transient(不持久化) strictfp
+         * enum assert
+         * goto const
+         */
+    }
+}
